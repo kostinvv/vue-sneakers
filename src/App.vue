4ff4194036fe6b88b8f6dcf41d4cad2e178eb489
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-14 09:47:13 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import CardItems from './components/CardItems.vue'
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <TheHeader />
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <CardItems />
+    </div>
+  </div>
+</template>
---
+++ src/components/CardItems.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SneakerCard from './SneakerCard.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <SneakerCard
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      image-url="/sneakers/sneakers-1.jpg"
+      :price="8000"
+      :is-added="true"
+      :is-favorite="true"
+    />
+    <SneakerCard />
+    <SneakerCard />
+    <SneakerCard />
+    <SneakerCard />
+  </div>
+</template>
